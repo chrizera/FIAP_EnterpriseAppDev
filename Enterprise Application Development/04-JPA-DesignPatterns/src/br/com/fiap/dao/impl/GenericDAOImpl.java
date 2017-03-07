36e61f2f44107fbd868c6e1f3a6c47d4dd954b65
logonrm <logonrm@fiap.com.br>
2017-03-06 22:55:41 -0300

    Update the generic dao method with example

--- Enterprise Application Development/04-JPA-DesignPatterns/src/br/com/fiap/dao/impl/GenericDAOImpl.java
+++ Enterprise Application Development/04-JPA-DesignPatterns/src/br/com/fiap/dao/impl/GenericDAOImpl.java
@@ -5,8 +5,10 @@ import java.lang.reflect.ParameterizedType;
 import javax.persistence.EntityManager;
 
 import br.com.fiap.dao.GenericDAO;
+import br.com.fiap.exception.CommitException;
+import br.com.fiap.exception.EntityNotFoundException;
 
-public class GenericDAOImpl<T, K> implements GenericDAO<T, K>{
+public abstract class GenericDAOImpl<T, K> implements GenericDAO<T, K> {
 	
 	private EntityManager em;
 	
@@ -18,24 +20,36 @@ public class GenericDAOImpl<T, K> implements GenericDAO<T, K>{
 	}
 	
 	@Override
-	public void insert(T entidade) {
-		em.getTransaction().begin();
+	public void insert(T entidade){
 		em.persist(entidade);
-		em.getTransaction().commit();
+	}
+
+	public void save() throws CommitException {
+		try {
+			em.getTransaction().begin();
+			em.getTransaction().commit();
+		}
+		catch(Exception e) {
+			if(em.getTransaction().isActive()) { //valida se existe uma transacao ativa
+				em.getTransaction().rollback();
+			}
+			e.printStackTrace();
+			throw new CommitException("Erro ao realizar o commit.");
+		}
 	}
 
 	@Override
 	public void update(T entidade) {
-		em.getTransaction().begin();
 		em.merge(entidade);
-		em.getTransaction().commit();
 	}
 
 	@Override
-	public void delete(K id) {
-		em.getTransaction().begin();
-		em.remove(findById(id));
-		em.getTransaction().commit();
+	public void delete(K id) throws EntityNotFoundException {
+		T entidade = findById(id);
+		if(entidade == null) {
+			throw new EntityNotFoundException("Entidade nao encontrada.");
+		}
+		em.remove(entidade);
 	}
 
 	@Override
